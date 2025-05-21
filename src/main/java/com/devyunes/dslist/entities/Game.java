5db7a6ab3106c40cd233d9b49433bba4de532c49
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-05-21 14:47:26 -0400

    Game seed

--- src/main/java/com/devyunes/dslist/entities/Game.java
+++ src/main/java/com/devyunes/dslist/entities/Game.java
@@ -20,24 +20,34 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	/* private Double score; */
+	private Double score;
 	private String imgUrl;
-	private String shorDescription;
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms/* Double score*/, String imgUrl,
-			String shorDescription, String longDescription) {
+	public Game(Long id,
+			String title,
+			Integer year,
+			String genre,
+			String platforms,
+			Double score,
+			String imgUrl,
+			String shorDescription,
+			String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
-		/* this.score = score; */
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shorDescription = shorDescription;
+		this.shortDescription = shorDescription;
 		this.longDescription = longDescription;
 	}
 
@@ -81,11 +91,13 @@ public class Game {
 		this.platforms = platforms;
 	}
 
-	/*
-	 * public Double getScore() { return score; }
-	 * 
-	 * public void setScore(Double score) { this.score = score; }
-	 */
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
 
 	public String getImgUrl() {
 		return imgUrl;
@@ -96,11 +108,11 @@ public class Game {
 	}
 
 	public String getShorDescription() {
-		return shorDescription;
+		return shortDescription;
 	}
 
 	public void setShorDescription(String shorDescription) {
-		this.shorDescription = shorDescription;
+		this.shortDescription = shorDescription;
 	}
 
 	public String getLongDescription() {
@@ -127,7 +139,5 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
